273d06c423855b06f52451db22d5967d33ca0573
Shirin <shirin@stzsoft.com>
2022-09-30 11:46:20 +0530

    changes in app

---
+++ application/logs/log-2022-09-30.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-09-30 10:45:51 --> Severity: error --> Exception: syntax error, unexpected 'public' (T_PUBLIC) C:\xampp\htdocs\legalmis\application\controllers\Sales.php 221
+ERROR - 2022-09-30 11:04:59 --> Severity: error --> Exception: syntax error, unexpected 'die' (T_EXIT), expecting function (T_FUNCTION) or const (T_CONST) C:\xampp\htdocs\legalmis\application\controllers\Sales.php 205
+ERROR - 2022-09-30 11:41:26 --> Severity: error --> Exception: Argument 1 passed to PHPExcel_IOFactory::createWriter() must be an instance of PHPExcel, null given, called in C:\xampp\htdocs\legalmis\application\controllers\Sales.php on line 224 C:\xampp\htdocs\legalmis\application\third_party\PHPExcel\IOFactory.php 129
+ERROR - 2022-09-30 11:45:14 --> Severity: error --> Exception: Call to a member function getWriter() on null C:\xampp\htdocs\legalmis\application\controllers\Sales.php 224
